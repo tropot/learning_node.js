d3010d7b3de54e2771ac0e35a0a307e6fde86250
tropot <mangustar123@gmail.com>
2022-10-27 00:26:54 +0300

    working with db

--- index.js
+++ index.js
@@ -4,14 +4,54 @@ const app = express();
 var nrIntrodus = 0;
 var nrRandom = rN(5);
 var suma = 0;
-console.log(rN(5));
+const mysql = require("mysql");
+const db = mysql.createConnection({
+
+    host: "localhost",
+  
+    user: "root",
+  
+    password: "",
+  
+    database: "testing",
+  
+  });
 app.get('/home', function(req, res) {
     res.sendFile(__dirname + "/home.html");
 })
 app.get('/form-submit', function(req, res) {
     nrIntrodus = parseInt(req.query.inputNumber);
     suma = nrIntrodus + nrRandom;
-    res.send(`suma numerelor :${suma}        Numarul random  ${nrRandom}         Numarul introdus: ${nrIntrodus}`);
+    console.log(typeof(nrIntrodus));
+    
+    
+    db.connect((err) => {
+
+        if (err) {
+      
+          throw err;
+      
+        }
+      
+        nrIntrodus = parseInt(nrIntrodus);
+        nrRandom = parseInt(nrRandom);
+        suma = parseInt(suma);
+        
+        var sql = "INSERT INTO numbers (input, random, sumaNr) VALUES (?,?,?)";
+        db.query(sql,[nrIntrodus,nrRandom,suma], function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+          });
+        if (err) throw err;
+        db.query("SELECT * FROM numbers", function (err, result, fields) {
+            if (err) throw err;
+            res.send(result);
+        });
+      
+      });
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('works on '))
+
+  
+  
